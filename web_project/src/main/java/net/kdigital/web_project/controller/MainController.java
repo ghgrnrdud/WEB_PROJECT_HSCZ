14070f1ba0de7c240f6311a31e4ef9a690ac6e9f
hyunseo <hyunseo1223@gmail.com>
2024-04-22 10:35:41 +0900

    Revert "Merge branch 'feature/stat_main' of https://github.com/DIMA0305/WEB_PROJECT into feature/code_hyunseo"
    This reverts commit 76ecdef038495943ab2abd1522a63a72c7ee07b9, reversing
    changes made to 18502565116ef29917f861afb1863956da52907a.

--- web_project/src/main/java/net/kdigital/web_project/controller/MainController.java
+++ web_project/src/main/java/net/kdigital/web_project/controller/MainController.java
@@ -2,15 +2,14 @@ package net.kdigital.web_project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 	
 	@GetMapping({"/",""})
-	public String sample(){
+	public String index(){
 		
-		return "sample-inner-page";
+		return "index";
 	}
 
 	@GetMapping("/layout")
