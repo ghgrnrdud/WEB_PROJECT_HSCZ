3aaf267cdeb67f94c471f337aff0a19883adfef0
BOOK-VFFVBU800U\Sehyeon111 <ssehn9327@gmail.com>
2024-04-29 14:34:04 +0900

    민서꺼 커밋

--- web_project/src/main/java/net/kdigital/web_project/controller/MainController.java
+++ web_project/src/main/java/net/kdigital/web_project/controller/MainController.java
@@ -6,13 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-<<<<<<< HEAD
-	@GetMapping({"","/"})
-	public String sample(){
-=======
+
 	@GetMapping({"/",""})
 	public String index(){
->>>>>>> d23e6147fd1e845ed68918b3c039f681e9cfae96
+
 		
 		// return "index";
 		return "index_jh";
