a2eddf89ac6ef824574bead24122a8480919b1ea
haneuljj <wjsckstj33@gmail.com>
2024-05-02 11:00:12 +0900

    5/2 메인페이지 관세사 순위에 관심품목 보이도록 설정

--- web_project/src/main/java/net/kdigital/web_project/controller/MainController.java
+++ web_project/src/main/java/net/kdigital/web_project/controller/MainController.java
@@ -1,6 +1,8 @@
 package net.kdigital.web_project.controller;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.kdigital.web_project.dto.CustomerDTO;
+import net.kdigital.web_project.dto.CustomerItemDTO;
+import net.kdigital.web_project.service.CustomerItemService;
 import net.kdigital.web_project.service.CustomerService;
 
 @Controller
@@ -17,14 +21,25 @@ import net.kdigital.web_project.service.CustomerService;
 public class MainController {
 
 	private final CustomerService customerService;
+	private final CustomerItemService customerItemService;
 
 	@GetMapping({ "/", "" })
 	public String index(
 			Model model) {
+
 		List<CustomerDTO> ccaList = customerService.selectTop3CCA();
+		Map<CustomerDTO, CustomerItemDTO>ccaInfoMap=new HashMap<>();		
+
+		for (int i = 0; i < ccaList.size(); i++) {
+			String userId = ccaList.get(i).getUserId();
+			CustomerItemDTO itemList = customerItemService.findItem(userId);
+
+			ccaInfoMap.put(ccaList.get(i), itemList);
+		}
 
 		log.info("====== ccaList: {}", ccaList);
 		model.addAttribute("ccaList", ccaList);
+		model.addAttribute("ccaInfoMap", ccaInfoMap);
 
 		return "index_jh";
 	}
